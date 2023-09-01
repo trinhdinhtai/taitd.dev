ad6027bb5001e4ce1bc2e350075170f006e5e3de
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-02 01:01:56 +0700

    refactor: change content layer import

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,18 +1,7 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import { makeSource } from "contentlayer/source-files";
+import { Post } from "./lib/contentlayer/post";
 
-export const Post = defineDocumentType(() => ({
-  name: "Post",
-  filePathPattern: `**/*.md`,
-  fields: {
-    title: { type: "string", required: true },
-    date: { type: "date", required: true },
-  },
-  computedFields: {
-    url: {
-      type: "string",
-      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
-    },
-  },
-}));
-
-export default makeSource({ contentDirPath: "posts", documentTypes: [Post] });
+export default makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Post],
+});

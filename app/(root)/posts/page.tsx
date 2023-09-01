988a822d801beeb0702b38a86f9aa8ca754dcfa5
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-02 00:35:31 +0700

    feat: add contentlayer

--- app/(root)/posts/page.tsx
+++ app/(root)/posts/page.tsx
@@ -1,7 +1,9 @@
+import { allPosts, Post } from "contentlayer/generated";
+
 const PostsPage = () => {
   return (
     <div className="container mb-4">
-      <div className="prose mx-auto max-w-5xl dark:prose-invert prose-headings:font-heading prose-headings:font-bold prose-headings:leading-tight hover:prose-a:text-accent-foreground prose-a:prose-headings:no-underline">
+      <div className="prose-headings:font-heading prose mx-auto max-w-5xl dark:prose-invert prose-headings:font-bold prose-headings:leading-tight hover:prose-a:text-accent-foreground prose-a:prose-headings:no-underline">
         <h1 className="mt-0">Latest Posts</h1>
         <hr className="my-4" />
       </div>
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { withContentlayer } = require("next-contentlayer");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -9,4 +11,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withContentlayer(nextConfig);

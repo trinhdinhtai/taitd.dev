689b3385752b42d153c6a6cb2a697d81fc2374e9
trinhdinhtai <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-10 16:03:35 +0700

    feat: add signature

---
+++ components/layout/footer.tsx
@@ -0,0 +1,11 @@
+import { Signature } from "../signature";
+
+const Footer = () => {
+  return (
+    <footer className="mx-auto flex max-w-6xl flex-col items-center gap-6 border-t py-6 pb-28 sm:pb-6">
+      <Signature />
+    </footer>
+  );
+};
+
+export default Footer;

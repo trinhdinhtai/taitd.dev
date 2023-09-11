04ae5345157b9ce381d52ef5660f9b6995e931fe
trinhdinhtai <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-12 01:21:22 +0700

    feat: change navbar links

--- constants/navbar-links.ts
+++ constants/navbar-links.ts
@@ -1,33 +1,5 @@
 import { ContentNavItem, NavItem } from "@/types";
 
-import { defaultAuthor, siteMetadata } from "@/lib/metadata";
-
-const blogContent: ContentNavItem[] = [
-  {
-    title: "Speaking",
-    href: "/speaking",
-    description:
-      "My previous (and current) talks, workshops, and other speaking engagements.",
-  },
-  {
-    title: "Videos",
-    href: defaultAuthor.socialProfiles.find(
-      (platform) => platform.name === "youtube",
-    )?.link as string,
-    description: "My YouTube channel where I talk about web development.",
-  },
-  {
-    title: "Newsletter",
-    href: siteMetadata.newsletterUrl as string,
-    description: "My newsletter about software development",
-  },
-  {
-    title: "Teaching",
-    href: "/teaching",
-    description: "I teach others. Sometimes for free, sometimes for money.",
-  },
-];
-
 const moreContent: ContentNavItem[] = [
   {
     title: "Uses",
@@ -49,10 +21,7 @@ export const navigationLinks: NavItem[] = [
     title: "Projects",
     href: "/projects",
   },
-  {
-    title: "Content",
-    content: blogContent,
-  },
+
   {
     title: "More",
     content: moreContent,

bd7754ac952ba2da67b646151ccf828948539379
trinhdinhtai <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-19 19:23:57 +0700

    feat: add metadata

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types"
+
+export const siteConfig: SiteConfig = {
+  name: "Taitd",
+  description:
+    "This website is my personal blog. I write about web development, JavaScript, TypeScript, React, Node.js, CSS, and more.",
+  url: "https://taitd.io.vn",
+  ogImage: "https://taitd.io.vn/og.jpg",
+  links: {
+    twitter: "https://twitter.com/taitddev",
+    github: "https://github.com/trinhdinhtai/next-blog",
+  },
+}

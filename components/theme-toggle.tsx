60694ea71b8f352a313064fedb221f2fdadf6050
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-20 11:43:05 +0700

    chore: update package version

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
+const { fontFamily } = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -79,4 +79,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
-};
+}

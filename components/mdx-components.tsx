b35e0d6147deb7ed5cf7dc16b5d2c11d5064c8c0
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-22 01:15:13 +0700

    fix: fix scroll margin top when click heading anchor

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -17,7 +17,7 @@ const components = {
   h2: ({ className, ...props }) => (
     <h2
       className={cn(
-        "mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0",
+        "mt-10 scroll-m-28 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0",
         className
       )}
       {...props}
@@ -26,7 +26,7 @@ const components = {
   h3: ({ className, ...props }) => (
     <h3
       className={cn(
-        "mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
+        "mt-8 scroll-m-28 text-2xl font-semibold tracking-tight",
         className
       )}
       {...props}
@@ -35,7 +35,7 @@ const components = {
   h4: ({ className, ...props }) => (
     <h4
       className={cn(
-        "mt-8 scroll-m-20 text-xl font-semibold tracking-tight",
+        "mt-8 scroll-m-28 text-xl font-semibold tracking-tight",
         className
       )}
       {...props}

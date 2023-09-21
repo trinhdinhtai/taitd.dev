2fec3bef2c3e745ae31d6040c2242adccc99cb26
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-22 01:05:01 +0700

    feat: change command menu

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -1,34 +1,101 @@
 "use client"
 
-import { toast } from "sonner"
+import { useCallback, useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { DialogProps } from "@radix-ui/react-alert-dialog"
+import { useTheme } from "next-themes"
 
+import { navbarLinks } from "@/config/navbarLinks"
 import { cn } from "@/lib/utils"
-import { Input } from "@/components/ui/input"
 
-interface SearchCommandProps extends React.HTMLAttributes<HTMLFormElement> {}
+import { Icons } from "./icons"
+import { Button } from "./ui/button"
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "./ui/command"
 
-const SearchCommand = ({ className, ...props }: SearchCommandProps) => {
-  function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault()
+const SearchCommand = ({ ...props }: DialogProps) => {
+  const router = useRouter()
+  const [open, setOpen] = useState(false)
+  const { setTheme } = useTheme()
 
-    return toast.success("Not implemented")
-  }
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
+    }
+
+    document.addEventListener("keydown", down)
+    return () => document.removeEventListener("keydown", down)
+  }, [])
+
+  const runCommand = useCallback((command: () => unknown) => {
+    setOpen(false)
+    command()
+  }, [])
 
   return (
-    <form
-      onSubmit={onSubmit}
-      className={cn("relative w-full", className)}
-      {...props}
-    >
-      <Input
-        type="search"
-        placeholder="Search blog..."
-        className="h-8 w-full sm:w-64 sm:pr-12"
-      />
-      <kbd className="pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-background px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100 sm:flex">
-        <span className="text-xs">⌘</span>K
-      </kbd>
-    </form>
+    <>
+      <Button
+        variant="outline"
+        className={cn(
+          "relative w-full items-center justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64"
+        )}
+        onClick={() => setOpen(true)}
+        {...props}
+      >
+        <span className="hidden lg:inline-flex">Search my blog...</span>
+        <span className="inline-flex lg:hidden">Search...</span>
+        <kbd className="pointer-events-none absolute right-1.5 top-2 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Links">
+            {navbarLinks.map((link) => (
+              <CommandItem
+                key={link.href}
+                value={link.title}
+                onSelect={() => {
+                  runCommand(() => router.push(link.href as string))
+                }}
+              >
+                {link.title}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+
+          <CommandSeparator />
+
+          <CommandGroup heading="Theme">
+            <CommandItem onSelect={() => runCommand(() => setTheme("light"))}>
+              <Icons.sun className="mr-2 h-4 w-4" />
+              Light
+            </CommandItem>
+            <CommandItem onSelect={() => runCommand(() => setTheme("dark"))}>
+              <Icons.moon className="mr-2 h-4 w-4" />
+              Dark
+            </CommandItem>
+            <CommandItem onSelect={() => runCommand(() => setTheme("system"))}>
+              <Icons.laptop className="mr-2 h-4 w-4" />
+              System
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
   )
 }
 

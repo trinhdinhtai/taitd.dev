d37005cef5c5a06d9f6798b6cb5137aab0d83ae3
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-26 14:25:13 +0700

    style: add transition and hover styles to improve user experience when interacting with table of contents links

--- components/toc.tsx
+++ components/toc.tsx
@@ -50,13 +50,13 @@ interface TreeProps {
 function Tree({ tree, level = 1, activeItem }: TreeProps) {
   return tree?.items?.length && level < 3 ? (
     <ul className={cn("m-0 list-none", { "pl-4": level !== 1 })}>
-      {tree.items.map((item, index) => {
+      {tree.items.map((item) => {
         return (
-          <li key={index} className={cn("mt-0 pt-2")}>
+          <li key={item.url} className={cn("mt-0 pt-2")}>
             <a
               href={item.url}
               className={cn(
-                "inline-block no-underline",
+                "inline-block no-underline transition-all hover:text-primary hover:underline",
                 item.url === `#${activeItem}`
                   ? "font-medium text-primary"
                   : "text-sm text-muted-foreground"

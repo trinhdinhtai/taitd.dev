3fda90d23de91f657fbc19f1f892772705c7ce20
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-26 16:26:31 +0700

    feat: add prisma

--- env.mjs
+++ env.mjs
@@ -3,15 +3,15 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-  // server: {
-  //   DATABASE_URL: z.string().url(),
-  //   OPEN_AI_API_KEY: z.string().min(1),
-  // },
+  server: {
+    DATABASE_URL: z.string().url().min(1),
+  },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:

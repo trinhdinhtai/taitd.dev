a6a708e942962c3ddbf3de4cb5706532b0d58420
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-27 09:12:27 +0700

    refactor: add a new logo image file (Bongo-Cat.png) to be used in the logo component

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,14 +1,19 @@
+import Image from "next/image"
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 
-import { Icons } from "./icons"
-
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center space-x-2">
-      <Icons.logo />
-      <span className="font-mono font-bold lowercase">{`${siteConfig.name}.dev()`}</span>
+    <Link href="/" className="relative flex items-center space-x-2">
+      <Image
+        width={90}
+        height={90}
+        alt="logo"
+        src="/images/Bongo-Cat.png"
+        className="absolute -right-8 -top-10"
+      />
+      <span className="font-mono text-lg font-bold lowercase">{`${siteConfig.name}.dev()`}</span>
     </Link>
   )
 }

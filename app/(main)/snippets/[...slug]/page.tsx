b9da592ef81d506650506a29783ffcf0015e11fc
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2023-09-29 00:21:42 +0700

    feat: added a new file `page.tsx` in the `app/(main)/snippets/[...slug]` directory to handle rendering of individual snippet pages.

---
+++ app/(main)/snippets/[...slug]/page.tsx
@@ -0,0 +1,82 @@
+import { notFound } from "next/navigation"
+import { allSnippets } from "@/.contentlayer/generated"
+
+import { formatDate } from "@/lib/utils"
+import Mdx from "@/components/mdx-components"
+
+import "@/styles/mdx.css"
+
+import { Metadata } from "next"
+
+import { env } from "@/env.mjs"
+
+const BASE_URL = env.NEXT_PUBLIC_APP_URL
+
+interface SnippetPageProps {
+  params: {
+    slug: string[]
+  }
+}
+
+export async function generateMetadata({
+  params,
+}: SnippetPageProps): Promise<Metadata> {
+  const post = await getSnippetFromParams(params)
+
+  if (!post) {
+    return {}
+  }
+
+  const { title, description } = post
+
+  const ogUrl = new URL(`${BASE_URL}/api/og`)
+  ogUrl.searchParams.set("heading", title)
+  ogUrl.searchParams.set("type", "Blog Post")
+  ogUrl.searchParams.set("mode", "dark")
+
+  return {
+    title: title,
+    description: description,
+  }
+}
+
+async function getSnippetFromParams(params: { slug: string[] }) {
+  const slug = params?.slug?.join("/")
+  const snippet = allSnippets.find((snippet) => snippet.slugAsParams === slug)
+
+  return snippet
+}
+
+const SnippetPage = async ({ params }: SnippetPageProps) => {
+  const snippet = await getSnippetFromParams(params)
+
+  if (!snippet) {
+    notFound()
+  }
+
+  return (
+    <article className="relative lg:gap-10 xl:grid xl:max-w-6xl">
+      <div className="w-full min-w-0">
+        <div>
+          <h1 className="mt-2 inline-block font-heading text-4xl leading-tight lg:text-5xl">
+            {snippet.title}
+          </h1>
+
+          <div className="mt-4 flex space-x-2 text-lg text-muted-foreground">
+            {snippet.lastUpdatedDate && (
+              <time dateTime={snippet.lastUpdatedDate}>
+                {formatDate(snippet.lastUpdatedDate)}
+              </time>
+            )}
+          </div>
+        </div>
+
+        <Mdx code={snippet.body.code} />
+
+        <hr className="my-4" />
+      </div>
+    </article>
+  )
+}
+
+export default SnippetPage

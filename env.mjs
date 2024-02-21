4d61fec7316c6953f6ca0c3fdbae8b86c04ff298
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2024-02-21 13:51:34 +0700

    feat: add DIRECT_URL to server environment variables and update Prisma datasource provider

--- env.mjs
+++ env.mjs
@@ -5,6 +5,7 @@ import { z } from "zod"
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url().min(1),
+    DIRECT_URL: z.string().url().min(1),
     NEXT_PUBLIC_RAINDROP_ACCESS_TOKEN: z.string().min(1),
     GITHUB_ACCESS_TOKEN: z.string().min(1),
     NEXT_PUBLIC_WAKATIME_ACCESS_TOKEN: z.string().min(1),
@@ -15,6 +16,7 @@ export const env = createEnv({
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_RAINDROP_ACCESS_TOKEN:
       process.env.NEXT_PUBLIC_RAINDROP_ACCESS_TOKEN,

ca9d059af107f6b27335466809fa1fd4c1368403
taitd-gms <117064867+taitd-gms@users.noreply.github.com>
2024-03-27 17:34:12 +0700

    fix: remove unused component

--- components/stats/stats-overview.tsx
+++
@@ -1,9 +0,0 @@
-export default function StatsOverview() {
-  return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-      {statCards.map((card) => (
-        <StatCard key={card.title} card={card} />
-      ))}
-    </div>
-  )
-}

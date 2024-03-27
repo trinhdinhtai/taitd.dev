516855a2b68a79c90948821d00c4f1792fadc20a
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2024-03-27 18:00:23 +0700

    feat: add db:setup script to package.json and create seed.ts script

---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+const { PrismaClient } = require("@prisma/client")
+
+const database = new PrismaClient()
+
+async function main() {
+  try {
+    await database.project.createMany({
+      data: [
+        {
+          title: "Personal portfolio",
+          description:
+            "My personal portfolio website, built with Next.js, Tailwind CSS, and TypeScript. It's a place where I can showcase my work and share my thoughts.",
+          imageUrl: "/images/projects/blog.png",
+          githubUrl: "https://github.com/trinhdinhtai/next-blog",
+        },
+      ],
+    })
+
+    console.log("Success")
+  } catch (error) {
+    console.log("Error seeding the database project", error)
+  } finally {
+    await database.$disconnect()
+  }
+}
+
+main()

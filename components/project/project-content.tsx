a4f55c01f7bb5dd4dc3665ef87c06e95e9245298
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2024-03-29 13:30:57 +0700

    feat: add project links and icons

--- components/project/project-content.tsx
+++ components/project/project-content.tsx
@@ -1,4 +1,7 @@
+import { Github, Share } from "lucide-react"
+
 import { EntireProject } from "@/types/project"
+import ProjectLink from "@/components/project/project-link"
 import ProjectStacks from "@/components/project/project-stacks"
 
 interface ProjectContentProps {
@@ -8,16 +11,43 @@ interface ProjectContentProps {
 export default function ProjectContent({
   project,
 }: Readonly<ProjectContentProps>) {
-  const { projectStack } = project
+  const { projectStack, githubUrl, demoUrl } = project
+
+  const projectLinks = [
+    {
+      title: "Source code",
+      url: githubUrl,
+      icon: <Github />,
+    },
+    {
+      title: "Live demo",
+      url: demoUrl,
+      icon: <Share />,
+    },
+  ]
   return (
     <div className="space-y-8">
       <div className="flex flex-col justify-between gap-5 sm:flex-row lg:items-center">
         <div className="flex flex-wrap items-center gap-3">
-          <span className="mb-1 text-[15px] text-neutral-700 dark:text-neutral-300">
+          <span className="mb-1 text-[15px] font-medium text-neutral-700 dark:text-neutral-300">
             Tech Stack :
           </span>
 
           <ProjectStacks projectStack={projectStack} />
+        </div>
+
+        <div className="flex items-center gap-4">
+          {projectLinks.map(
+            (link) =>
+              link.url && (
+                <ProjectLink
+                  key={link.title}
+                  title={link.title}
+                  url={link.url}
+                  icon={link.icon}
+                />
+              )
+          )}
         </div>
       </div>
     </div>
---
+++ components/project/project-link.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react"
+import Link from "next/link"
+
+interface ProjectLinksProps {
+  title: string
+  url: string
+  icon?: ReactNode
+}
+
+export default function ProjectLink({
+  title,
+  url,
+  icon,
+}: Readonly<ProjectLinksProps>) {
+  return (
+    <Link href={url} target="_blank">
+      <div className="flex items-center gap-2 font-medium text-neutral-700 dark:text-neutral-300 ">
+        {icon}
+        <span className="text-[15px] capitalize transition-all duration-300 dark:text-teal-500 hover:dark:text-teal-400">
+          {title}
+        </span>
+      </div>
+    </Link>
+  )
+}

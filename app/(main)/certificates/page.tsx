134a052ddd6376d55152271c2fe959709ae410d1
trinhdinhtai <142890841+trinhdinhtai@users.noreply.github.com>
2024-03-31 00:59:09 +0700

    feat: refactor CertificatesPage component to fetch and display certificates from the database

--- app/(main)/certificates/page.tsx
+++ app/(main)/certificates/page.tsx
@@ -1,3 +1,28 @@
-export default function CertificatesPage() {
-  return <div>CertificatesPage</div>
+import { Metadata } from "next"
+
+import { prisma } from "@/lib/prisma"
+import Certificates from "@/components/certificates"
+import PageHeading from "@/components/page-heading"
+
+export const metadata: Metadata = {
+  title: "Certificates",
+}
+
+export default async function CertificatesPage() {
+  const certificates = await prisma.certificate.findMany({
+    orderBy: {
+      issueDate: "desc",
+    },
+  })
+
+  return (
+    <>
+      <PageHeading
+        title="Certificates"
+        description="My certificates and achievements."
+      />
+
+      <Certificates certificates={certificates} />
+    </>
+  )
 }

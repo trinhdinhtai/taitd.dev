c98103ed8c6ede0502d1d5d7768ef412b9da122e
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2024-04-03 21:53:44 +0700

    feat: update NotFound component to display an image and error message

--- app/(main)/not-found.tsx
+++ app/(main)/not-found.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image"
+
 export default function NotFound() {
   return (
-    <div className="flex h-full items-center justify-center">Not found</div>
+    <div className="flex min-h-[60vh] flex-col items-center justify-center">
+      <Image
+        width={80}
+        height={80}
+        src="/images/searching-duck.gif"
+        alt="Yellow duck searching"
+      />
+      <h1 className="mb-3 mt-6 text-3xl font-black leading-tight tracking-tight sm:text-6xl lg:leading-[3.7rem]">
+        Error 404!
+      </h1>
+      <p className="max-w-2xl text-base leading-relaxed text-zinc-600 dark:text-zinc-400">
+        Oop! This page does not exist.
+      </p>
+    </div>
   )
 }

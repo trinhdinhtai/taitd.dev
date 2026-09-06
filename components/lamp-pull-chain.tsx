b2bb73fd3beb37106a35fa557e2901d363c10c9e
trinhdinhtai <142890841+trinhdinhtai@users.noreply.github.com>
2026-09-06 14:12:11 +0700

    feat: add handwritten note and arrow components to enhance theme toggle interaction in LampPullChain

--- components/lamp-pull-chain.tsx
+++ components/lamp-pull-chain.tsx
@@ -4,6 +4,10 @@ import { useTheme } from "next-themes"
 
 import { useClickSound } from "@/hooks/soundcn/use-click-sound"
 import { useIsClient } from "@/hooks/use-is-client"
+import {
+  HandwrittenArrow,
+  HandwrittenNote,
+} from "@/features/blog/components/handwritten-note"
 
 import { PullCord } from "./pull-cord"
 
@@ -24,11 +28,25 @@ function LampPullChain() {
   }
 
   return (
-    <PullCord
-      onPull={() => onSwitchTheme()}
-      pulled={isClient && resolvedTheme === "light"}
-      ariaLabel="Toggle theme"
-    />
+    <>
+      <PullCord
+        onPull={() => onSwitchTheme()}
+        pulled={isClient && resolvedTheme === "light"}
+        ariaLabel="Toggle theme"
+      />
+
+      <HandwrittenNote
+        className="pullcord fixed top-52 right-[calc(var(--pullcord-right)+3.25rem)] z-60 hidden w-32 flex-col items-end pointer-fine:xl:flex"
+        aria-hidden
+      >
+        <HandwrittenArrow className="-scale-x-100 -scale-y-100 rotate-12" />
+        <span className="mr-1 -rotate-6 text-right">
+          pull to toggle
+          <span className="block" />
+          light / dark
+        </span>
+      </HandwrittenNote>
+    </>
   )
 }
 
